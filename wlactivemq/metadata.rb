21f839d65b30d97973b15ca4332fee89f69d4e56
gidbalugo <zidgyril@gmail.com>
2016-10-05 17:57:44 +0800

    create wlactivemq cookbook

---
+++ wlactivemq/metadata.rb
@@ -0,0 +1,7 @@
+name 'wlactivemq'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures wlactivemq'
+long_description 'Installs/Configures wlactivemq'
+version '0.1.0'
