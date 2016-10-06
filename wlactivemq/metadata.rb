c9cb352d1eda468c030e1eab91b9b928834a2741
gidbalugo <zidgyril@gmail.com>
2016-10-06 20:21:55 +0800

    activemq recipe and added dependency on metadata

--- wlactivemq/metadata.rb
+++ wlactivemq/metadata.rb
@@ -4,4 +4,11 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures wlactivemq'
 long_description 'Installs/Configures wlactivemq'
-version '0.1.0'
+version          "0.1.0"
+
+%w{ubuntu debian redhat centos}.each do |os|
+  supports os
+end
+
+depends 'java'
+
