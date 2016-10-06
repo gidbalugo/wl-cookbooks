a3a515c3d39f28c2ed3e7a4e843c0dcab38e9d30
gidbalugo <zidgyril@gmail.com>
2016-10-06 20:22:12 +0800

    added values for node

---
+++ wlactivemq/attributes/default.rb
@@ -0,0 +1,17 @@
+#
+#
+#
+#
+
+
+default['activemq']['mirror']  = "http://apache.mirrors.tds.net"
+default['activemq']['version'] = "5.13.4"
+default['activemq']['home']  = "/opt"
+default['activemq']['wrapper']['max_memory'] = "512"
+default['activemq']['wrapper']['useDedicatedTaskRunner'] = "true"
+
+default['activemq']['zooKeeper']['address'] = "localhost"
+default['activemq']['zooKeeper']['hostname'] = "localhost"
+
+override['java']['install_flavor'] = "openjdk"
+override['java']['jdk_version'] = "7"
