c512693c246f7f8c5c7462679859adc4dd610216
gidbalugo <zidgyril@gmail.com>
2016-10-12 18:11:09 +0800

    updated ip

--- wlzookeeper/attributes/default.rb
+++ wlzookeeper/attributes/default.rb
@@ -11,15 +11,15 @@ default['zookeeper']['dataDir'] = "/var/zookeeper"
 #zookeeper ids - values do not necessarily comply with id-X
 #zookeeper address are from the created instance in aws
 default['zookeeper']['id1'] 	  = "1"
-default['zookeeper']['address1'] = "54.197.36.162"
+default['zookeeper']['address1'] = "54.210.67.63"
 default['zookeeper']['private1'] = "172.31.38.64"
 
 default['zookeeper']['id2']	  = "2"
-default['zookeeper']['address2'] = "54.166.52.11"
+default['zookeeper']['address2'] = "54.196.56.137"
 default['zookeeper']['private2'] = "172.31.36.224"
 
 default['zookeeper']['id3'] 	  = "3"
-default['zookeeper']['address3'] = "54.172.199.170"
+default['zookeeper']['address3'] = "54.209.223.245"
 default['zookeeper']['private3'] = "172.31.47.51"
 
 #default myid is. this is updated by recipe depending on private ip
