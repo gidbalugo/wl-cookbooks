7cc8d98fdc331f759b5223a3e8fa5c712f04caff
gidbalugo <zidgyril@gmail.com>
2016-10-13 21:33:01 +0800

    fix error in separating port and actual address

--- wlactivemq/attributes/default.rb
+++ wlactivemq/attributes/default.rb
@@ -6,9 +6,9 @@ default['activemq']['wrapper']['useDedicatedTaskRunner'] = "true"
 
 #zookeeper addresses
 default['activemq']['zooKeeper']['port'] = "2181"
-default['activemq']['zooKeeper']['address1'] = "54.205.121.180:2181"
-default['activemq']['zooKeeper']['address2'] = "54.88.31.216:2181"
-default['activemq']['zooKeeper']['address3'] = "54.161.93.138:2181"
+default['activemq']['zooKeeper']['address1'] = "54.205.121.180"
+default['activemq']['zooKeeper']['address2'] = "54.88.31.216"
+default['activemq']['zooKeeper']['address3'] = "54.161.93.138"
 
 #default to 0.0.0.0
 default['activemq']['hostname'] = "0.0.0.0"
