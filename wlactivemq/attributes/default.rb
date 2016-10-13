a690819a56bae3b03bf5123eac3958a670f7c18a
gidbalugo <zidgyril@gmail.com>
2016-10-13 21:23:53 +0800

    updated ip and added hostname for dynamic assignment of hosted activemq

--- wlactivemq/attributes/default.rb
+++ wlactivemq/attributes/default.rb
@@ -1,14 +1,14 @@
-#
-#
-#
-#
-
-
 default['activemq']['mirror']  = "http://apache.mirrors.tds.net"
 default['activemq']['version'] = "5.13.4"
 default['activemq']['home']  = "/opt"
 default['activemq']['wrapper']['max_memory'] = "512"
 default['activemq']['wrapper']['useDedicatedTaskRunner'] = "true"
 
-default['activemq']['zooKeeper']['address'] = "54.197.36.162:2181"
-default['activemq']['zooKeeper']['hostname'] = "54.197.36.162"
+#zookeeper addresses
+default['activemq']['zooKeeper']['port'] = "2181"
+default['activemq']['zooKeeper']['address1'] = "54.205.121.180:2181"
+default['activemq']['zooKeeper']['address2'] = "54.88.31.216:2181"
+default['activemq']['zooKeeper']['address3'] = "54.161.93.138:2181"
+
+#default to 0.0.0.0
+default['activemq']['hostname'] = "0.0.0.0"
