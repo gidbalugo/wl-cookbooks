15b6efffc8390656023f991f3ebed0f74a44e119
gidbalugo <zidgyril@gmail.com>
2016-10-14 16:23:17 +0800

    updated with test ip of zookeeper

--- wlactivemq/attributes/default.rb
+++ wlactivemq/attributes/default.rb
@@ -6,9 +6,9 @@ default['activemq']['wrapper']['useDedicatedTaskRunner'] = "true"
 
 #zookeeper addresses
 default['activemq']['zooKeeper']['port'] = "2181"
-default['activemq']['zooKeeper']['address1'] = "54.205.121.180"
-default['activemq']['zooKeeper']['address2'] = "54.88.31.216"
-default['activemq']['zooKeeper']['address3'] = "54.161.93.138"
+default['activemq']['zooKeeper']['address1'] = "54.161.168.126"
+default['activemq']['zooKeeper']['address2'] = "54.173.130.21"
+default['activemq']['zooKeeper']['address3'] = "54.159.109.248"
 
 #default to 0.0.0.0
 default['activemq']['hostname'] = "0.0.0.0"
