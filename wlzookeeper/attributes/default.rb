8ac0f7265049c8a2a9cf05bb19105ccd367ebb01
gidbalugo <zidgyril@gmail.com>
2016-10-17 14:13:07 +0800

    udpated test ip

--- wlzookeeper/attributes/default.rb
+++ wlzookeeper/attributes/default.rb
@@ -12,19 +12,19 @@ default['zookeeper']['dataDir'] = "/var/zookeeper"
 #zookeeper address are from the created instance in aws
 default['zookeeper']['id1'] 	  			= "1"
 default['zookeeper']['private1'] 			= "172.31.41.188"
-default['zookeeper']['instance']['address1']= "54.161.168.126"
-default['zookeeper']['config']['address1'] 	= "54.161.168.126"
+default['zookeeper']['instance']['address1']= "52.201.217.58"
+default['zookeeper']['config']['address1'] 	= "52.201.217.58"
 
 
 default['zookeeper']['id2']	  				= "2"
 default['zookeeper']['private2'] 			= "172.31.34.123"
-default['zookeeper']['instance']['address2']= "54.173.130.21"
-default['zookeeper']['config']['address2'] 	= "54.173.130.21"
+default['zookeeper']['instance']['address2']= "54.86.52.71"
+default['zookeeper']['config']['address2'] 	= "54.86.52.71"
 
 default['zookeeper']['id3'] 	  			= "3"
 default['zookeeper']['private3'] 			= "172.31.34.129"
-default['zookeeper']['instance']['address3']= "54.159.109.248"
-default['zookeeper']['config']['address3']  = "54.159.109.248"
+default['zookeeper']['instance']['address3']= "54.234.16.192"
+default['zookeeper']['config']['address3']  = "54.234.16.192"
 
 #default myid for config is 1 or whatever
 default['zookeeper']['myid'] = "1"
