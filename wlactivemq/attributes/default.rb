5f6f006d053c47485e34d57632a109d58c9ba84e
gidbalugo <zidgyril@gmail.com>
2016-10-19 21:30:18 +0800

    added private ip of zookeepers for activemq config

--- wlactivemq/attributes/default.rb
+++ wlactivemq/attributes/default.rb
@@ -6,9 +6,9 @@ default['activemq']['wrapper']['useDedicatedTaskRunner'] = "true"
 
 #zookeeper addresses
 default['activemq']['zooKeeper']['port'] = "2181"
-default['activemq']['zooKeeper']['address1'] = "52.201.217.58"
-default['activemq']['zooKeeper']['address2'] = "54.86.52.71"
-default['activemq']['zooKeeper']['address3'] = "54.234.16.192"
+default['activemq']['zooKeeper']['address1'] = "10.8.1.183"
+default['activemq']['zooKeeper']['address2'] = "10.8.3.152"
+default['activemq']['zooKeeper']['address3'] = "10.8.1.53"
 
 #default to 0.0.0.0
 default['activemq']['hostname'] = "0.0.0.0"
